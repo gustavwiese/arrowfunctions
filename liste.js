63aa45b37b496acf341ccdfbcc662cc5fc0fd906
gustavwiese <gusse12345@gmail.com>
2023-05-01 09:01:37 +0200

    Initial commit

---
+++ liste.js
@@ -0,0 +1,11 @@
+const liste = [
+    { firstName: "Harry", lastName: "Potter", house: "Gryffindor", gender: "male" },
+    { firstName: "Ron", lastName: "Weasley", house: "Gryffindor", gender: "male" },
+    { firstName: "Hermione", lastName: "Granger", house: "Gryffindor", gender: "female" },
+    { firstName: "Draco", lastName: "Malfoy", house: "Slytherin", gender: "male" },
+    { firstName: "Pansy", lastName: "Parkinson", house: "Slytherin", gender: "female" },
+    { firstName: "Hannah", lastName: "Abbott", house: "Hufflepuff", gender: "female" },
+    { firstName: "Justin", lastName: "Finch-Fletchley", house: "Hufflepuff", gender: "male" },
+    { firstName: "Michael", lastName: "Corner", house: "Ravenclaw", gender: "male" },
+    { firstName: "Padma", lastName: "Patil", house: "Ravenclaw", gender: "female" },
+];
