1aa537e4224c22f7ab3ec4a66e3358c92717e37f
gustavwiese <gusse12345@gmail.com>
2023-05-01 09:53:59 +0200

    excercise 1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JavaScript kører");
+
+  console.log(isGryffindor(liste[0]));
+  console.log(isNotGryffindor(liste[0]));
+  console.log(hasLastName(liste[0], "potter"));
+}
+
+function isGryffindor(student) {
+  return student.house.toLowerCase() == "gryffindor";
+}
+
+function isNotGryffindor(student) {
+  return student.house.toUpperCase() !== "GRYFFINDOR";
+}
+
+function hasLastName(student, lastName) {
+  return student.lastName.toLowerCase().trim() == lastName;
+}
